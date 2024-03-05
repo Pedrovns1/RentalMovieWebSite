c8412581fe29e6d759b616ed0b39375de69386b5
Pedro Vinicius <peuvini.94@gmail.com>
2024-03-05 16:54:07 +0000

    Update Payments page

---
+++ Payments.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    var exitButton = document.querySelector('.exit-btn');
+
+    exitButton.addEventListener('click', function () {
+
+        window.location.href = "CA1 Web Dev.html"; 
+    });
+});
